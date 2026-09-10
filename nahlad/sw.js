bff996198009bdc3711a94f542cfbfb916015777
Ivan Kurilec <276676276+ikurilec@users.noreply.github.com>
2026-09-10 18:50:54 +0200

    v2.85.7: isolate line switch data

--- nahlad/sw.js
+++ nahlad/sw.js
@@ -4,7 +4,7 @@
 // ║  cache-first pre ostatné assety (rýchly štart, offline ready).║
 // ╚══════════════════════════════════════════════════════════════╝
 
-var CACHE_NAME = 'potencial-nahlad-v2856';
+var CACHE_NAME = 'potencial-nahlad-v2857';
 
 self.addEventListener('install', function(event){
   self.skipWaiting();
