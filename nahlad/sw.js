7caa12024d50e62047aed0ab184f2707468fab1f
Ivan Kurilec <276676276+ikurilec@users.noreply.github.com>
2026-09-10 19:13:40 +0200

    v2.85.8: fix login and line loading

--- nahlad/sw.js
+++ nahlad/sw.js
@@ -4,7 +4,7 @@
 // ║  cache-first pre ostatné assety (rýchly štart, offline ready).║
 // ╚══════════════════════════════════════════════════════════════╝
 
-var CACHE_NAME = 'potencial-nahlad-v2857';
+var CACHE_NAME = 'potencial-nahlad-v2858';
 
 self.addEventListener('install', function(event){
   self.skipWaiting();
