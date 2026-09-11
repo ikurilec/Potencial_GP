f0c7cdc1621ca4939ca88ae542e5fe80529489e5
Ivan Kurilec <276676276+ikurilec@users.noreply.github.com>
2026-09-11 10:38:05 +0200

    v2.85.22: fix Golem form layout

--- nahlad/sw.js
+++ nahlad/sw.js
@@ -4,7 +4,7 @@
 // ║  cache-first pre ostatné assety (rýchly štart, offline ready).║
 // ╚══════════════════════════════════════════════════════════════╝
 
-var CACHE_NAME = 'potencial-nahlad-v28521';
+var CACHE_NAME = 'potencial-nahlad-v28522';
 
 self.addEventListener('install', function(event){
   self.skipWaiting();
