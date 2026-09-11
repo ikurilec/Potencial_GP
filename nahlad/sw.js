39b746e64bddca662eaf63555973bf6bc5a5b643
Ivan Kurilec <276676276+ikurilec@users.noreply.github.com>
2026-09-11 14:35:33 +0200

    v2.85.36: split cached app assets

--- nahlad/sw.js
+++ nahlad/sw.js
@@ -4,7 +4,7 @@
 // ║  cache-first pre ostatné assety (rýchly štart, offline ready).║
 // ╚══════════════════════════════════════════════════════════════╝
 
-var CACHE_NAME = 'potencial-nahlad-v28535';
+var CACHE_NAME = 'potencial-nahlad-v28536';
 
 self.addEventListener('install', function(event){
   self.skipWaiting();
