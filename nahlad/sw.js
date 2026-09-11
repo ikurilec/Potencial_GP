cf9d3d58c1afc1fe7d6857b3128e7c84820b263b
Ivan Kurilec <276676276+ikurilec@users.noreply.github.com>
2026-09-11 10:48:01 +0200

    v2.85.23: include Lonelix doctors in GP update

--- nahlad/sw.js
+++ nahlad/sw.js
@@ -4,7 +4,7 @@
 // ║  cache-first pre ostatné assety (rýchly štart, offline ready).║
 // ╚══════════════════════════════════════════════════════════════╝
 
-var CACHE_NAME = 'potencial-nahlad-v28522';
+var CACHE_NAME = 'potencial-nahlad-v28523';
 
 self.addEventListener('install', function(event){
   self.skipWaiting();
