31f2b6ced5d339e9cedbe0596a235b297d686d37
Ivan Kurilec <276676276+ikurilec@users.noreply.github.com>
2026-09-11 11:52:20 +0200

    v2.85.26: načítať Gyn domov pred zobrazením

--- nahlad/sw.js
+++ nahlad/sw.js
@@ -4,7 +4,7 @@
 // ║  cache-first pre ostatné assety (rýchly štart, offline ready).║
 // ╚══════════════════════════════════════════════════════════════╝
 
-var CACHE_NAME = 'potencial-nahlad-v28525';
+var CACHE_NAME = 'potencial-nahlad-v28526';
 
 self.addEventListener('install', function(event){
   self.skipWaiting();
